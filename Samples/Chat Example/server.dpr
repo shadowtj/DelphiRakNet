9002b408c2eebd51ac3a82222f7d162cc7c60cbb
shadowtj <vbemmelstraat88>
2018-08-03 19:39:02 +0200

    First Commit

---
+++ Samples/Chat Example/server.dpr
@@ -0,0 +1,39 @@
+program server;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  System.SysUtils,
+  RakPeerInterface in '..\..\Source\RakPeerInterface.pas';
+
+var
+  portString: string;
+begin
+  try
+    writeln('This is a sample implementation of a text based chat server.');
+    writeln('Connect to the project "Chat Example Client"');
+    writeln('Difficulty: Beginner');
+    writeln('');
+
+    // A server
+    writeln('Enter the server port to listen on');
+    Readln(portString);
+
+    if portString = EmptyStr then
+      portString := '1234';
+
+    writeln('Starting server.');
+
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
